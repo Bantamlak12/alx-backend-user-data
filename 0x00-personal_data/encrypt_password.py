09aee11351785708f3c8248d05720aff73d9c164
Bantamlak <bantamlak29@gmail.com>
2024-02-08 18:31:38 +0300

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -15,3 +15,12 @@ def hash_password(password: str) -> bytes:
     """
     py_byte = password.encode()
     return bcrypt.hashpw(py_byte, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    parameters:
+    - hashed_password (bytes): A hashed password
+    - password (str): A password to check it with
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
