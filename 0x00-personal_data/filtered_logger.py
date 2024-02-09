7dc4194baf7a1f00322970cce3efa72bde5b9b00
Bantamlak <bantamlak29@gmail.com>
2024-02-09 10:15:15 +0300

    back to environ.get from os

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -36,10 +36,10 @@ def get_db() -> MySQLConnection:
     """ Returns a connector to the database
     """
     # Retrieving the values of an enviroment variable with default values
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.environ.get("PERSONAL_DATA_DB_NAME")
 
     connection = MySQLConnection(user=username, password=password,
                                  host=host, database=db_name)
