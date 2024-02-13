176d8a2ebedce105cc37c669e757c113632f2e0b
Bantamlak <bantamlak29@gmail.com>
2024-02-13 09:36:32 +0300

    handled any exception related to users

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -64,9 +64,11 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
         # Database Lookup
-        users = User.search({'email': user_email})
-        if not users:
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
             return None
+        # Check password for the user
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
