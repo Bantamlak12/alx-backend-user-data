6c38f6ecefdb579ee683019fa275f329edd6859b
Bantamlak <bantamlak29@gmail.com>
2024-02-14 12:40:43 +0300

    task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" A module that manages session authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Manage session authentication
+    """
+    pass
