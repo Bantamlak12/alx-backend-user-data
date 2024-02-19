9b8c65279b81ec1d32be29bc0cd0b759f07fe74e
Bantamlak <bantamlak29@gmail.com>
2024-02-19 15:47:20 +0300

    task 2: find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base
 from user import User
@@ -43,4 +45,22 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Find a user in a database based on the keyword argument.
+
+        Args:
+        - **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+        - The first row of the user found in the table.
+
+        Raises:
+        - NoResultFound: If no user found.
+        - InvalidRequestError: When wrong query argument are passed.
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
         return user
