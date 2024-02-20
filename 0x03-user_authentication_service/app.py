ec0787df6549a0207e54776fd4f5f0e75250ca74
Bantamlak <bantamlak29@gmail.com>
2024-02-20 09:49:20 +0300

    register user: status code 201 removed

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,7 +29,7 @@ def register():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
-    return jsonify({"email": email, "message": "user created"}), 201
+    return jsonify({"email": email, "message": "user created"})
 
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
